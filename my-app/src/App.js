7c4b13430b1decff8b40f398e65ad4c17464dd40
Shafna N <shafna.n@experionglobal.local>
2023-01-27 11:59:39 +0530

    initial commit

---
+++ my-app/src/App.js
@@ -0,0 +1,17 @@
+import Application from './components/Memo';
+import Test from './components/CallBack';
+import './App.css';
+
+function App() {
+  return (
+   <>
+   
+   <Application></Application>
+   <Test></Test>
+
+   </>
+ 
+  );
+}
+
+export default App;
